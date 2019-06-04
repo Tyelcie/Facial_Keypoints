08d0fbc8ca71cfee13413f9c3a8f3721ae7ab63e
Tyelcie <tyelcie@gmail.com>
2019-06-04 23:18:31 +0800

    structure

--- models.py
+++ models.py
@@ -23,10 +23,15 @@ class Net(nn.Module):
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        self.bn1 = nn.BatchNorm2d(32)
         self.conv2 = nn.Conv2d(32, 128, 3)
+        self.bn2 = nn.BatchNorm2d(128)
         self.conv3 = nn.Conv2d(128, 256, 2)
+        self.bn3 = nn.BatchNorm2d(256)
         self.conv4 = nn.Conv2d(256, 512, 2)
+        self.bn4 = nn.BatchNorm2d(512)
         self.conv5 = nn.Conv2d(512, 1024, 2)
+        self.bn5 = nn.BatchNorm2d(1024)
 #         self.pool = nn.MaxPool2d(2, 2)
         self.pool = nn.AvgPool2d(2, 2)
         self.fc1 = nn.Linear(25600, 12800)
@@ -40,22 +45,19 @@ class Net(nn.Module):
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
         
-        x = self.pool(F.relu(self.conv1(x)))
-        
-        x = self.pool(F.relu(self.conv2(x)))
-        
-        x = self.pool(F.relu(self.conv3(x)))
-        
-        x = self.pool(F.relu(self.conv4(x)))
-        
-        x = self.pool(F.relu(self.conv5(x)))
+        x = self.pool(F.leaky_relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.leaky_relu(self.bn2(self.conv2(x))))
+        x = self.pool(F.leaky_relu(self.bn3(self.conv3(x))))
+        x = self.pool(F.leaky_relu(self.bn4(self.conv4(x))))
+        x = self.pool(F.leaky_relu(self.bn5(self.conv5(x))))
         
         x = self.drop1(x)
         
         x = x.view(x.size(0), -1) # Flatten. ref: https://github.com/pytorch/pytorch/issues/2118
 #         print("Flatten size: ", x.shape)
         
-        x = self.drop2(F.relu(self.fc1(x)))
+        x = F.relu(self.fc1(x))
+        x = self.drop2(x)
         
         x = self.fc2(x)
         
